fc0b6b52f9a30dec81afaf441460eff1c57884bc
Pooja Patel <poojapatel.web@gmail.com>
2021-07-29 17:17:18 -0400

    word Search solver

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,16 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const transposed = letters[0].map((item, index) => letters.map(row => row[index]))
+    const verticalJoin = transposed.map(ls => ls.join(''))
+    for (v of verticalJoin) {
+        if (v.includes(word))
+            return true;
+    }
+    return false;
 }
-
-module.exports = wordSearch
+// Check to find the word horizontally and vertically
+// Return true if the word is found, and return false if the word is not found
+module.exports = wordSearch;
